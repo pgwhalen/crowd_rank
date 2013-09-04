e8e851f36810154b698a2f18a0727e3ff006d078
Paul Whalen <pgwhalen@gmail.com>
2013-09-04 00:00:31 -0400

    add nhl teams to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,30 +11,37 @@ Team.create([{ full_name: 'Buffalo Bills', location: 'Buffalo', nickname: 'Saint
 						 { full_name: 'Miami Dolphins', location: 'Miami', nickname: 'Dolphins' },
 						 { full_name: 'New England Patriots', location: 'New England', nickname: 'Patriots' },
 						 { full_name: 'New York Jets', location: 'New York', nickname: 'Jets' },
+
 						 { full_name: 'Baltimore Ravens', location: 'Baltimore', nickname: 'Ravens' },
 						 { full_name: 'Cincinnati Bengals', location: 'Cincinnati', nickname: 'Bengals' },
 						 { full_name: 'Cleveland Browns', location: 'Cleveland', nickname: 'Browns' },
 						 { full_name: 'Pittsburgh Steelers', location: 'Pittsburgh', nickname: 'Steelers' },
+
 						 { full_name: 'Houston Texans', location: 'Houston', nickname: 'Texans' },
 						 { full_name: 'Indianapolis Colts', location: 'Indianapolis', nickname: 'Colts' },
 						 { full_name: 'Jacksonville Jaguars', location: 'Jacksonville', nickname: 'Jaguars' },
 						 { full_name: 'Tennessee Titans', location: 'Tennessee', nickname: 'Titans' },
+
 						 { full_name: 'Denver Broncos', location: 'Denver', nickname: 'Broncos' },
 						 { full_name: 'Kansas City Chiefs', location: 'Kansas City', nickname: 'Chiefs' },
 						 { full_name: 'Oakland Raiders', location: 'Oakland', nickname: 'Raiders' },
 						 { full_name: 'San Diego Chargers', location: 'San Diego', nickname: 'Chargers' },
+
 						 { full_name: 'Dallas Cowboys', location: 'Dallas', nickname: 'Cowboys' },
 						 { full_name: 'New York Giants', location: 'New York', nickname: 'Giants' },
 						 { full_name: 'Philadelphia Eagles', location: 'Philadelphia', nickname: 'Eagles' },
 						 { full_name: 'Washington Redskins', location: 'Washington', nickname: 'Washington' },
+
 						 { full_name: 'Chicago Bears', location: 'Chicago', nickname: 'Bears' },						 
 						 { full_name: 'Detroit Lions', location: 'Detroit', nickname: 'Lions' },
 						 { full_name: 'Green Bay Packers', location: 'Green Bay', nickname: 'Packers' },
 						 { full_name: 'Minnesota Vikings', location: 'Minnesota', nickname: 'Vikings' },
+
 						 { full_name: 'Atlanta Falcons', location: 'Atlanta', nickname: 'Falcons' },
 						 { full_name: 'Carolina Panthers', location: 'Carolina', nickname: 'Panthers' },
 						 { full_name: 'New Orleans Saints', location: 'New Orleans', nickname: 'Saints' },
 						 { full_name: 'Tampa Bay Buccaneers', location: 'Tampa Bay', nickname: 'Buccaneers' },
+
 						 { full_name: 'Arizona Cardinals', location: 'Arizona', nickname: 'Cardinals' },
 						 { full_name: 'St. Louis Rams', location: 'St. Louis', nickname: 'Rams' },
 						 { full_name: 'San Fransisco 49ers', location: 'San Fransisco', nickname: '49ers' },
@@ -45,7 +52,41 @@ Team.create([{ full_name: 'Buffalo Bills', location: 'Buffalo', nickname: 'Saint
 # TODO
 
 # NHL Teams
-# TODO
+Team.create([{ full_name: 'Anaheim Ducks', location: 'Anaheim', nickname: 'Ducks' },
+						 { full_name: 'Calgary Flames', location: 'Calgary', nickname: 'Flames' },
+						 { full_name: 'Edmonton Oilers', location: 'Edmonton', nickname: 'Oilers' },
+						 { full_name: 'Los Angeles Kings', location: 'Los Angeles', nickname: 'Kings' },
+						 { full_name: 'Phoenix Coyotes', location: 'Phoenix', nickname: 'Coyotes' },
+						 { full_name: 'San Jone Sharks', location: 'San Jose', nickname: 'Sharks' },
+						 { full_name: 'Vancouver Canucks', location: 'Vancouver', nickname: 'Canucks' },
+
+						 { full_name: 'Chicago Blackhawks', location: 'Chicago', nickname: 'Blackhawks' },
+						 { full_name: 'Colorado Avalanche', location: 'Colorado', nickname: 'Avalanche' },
+						 { full_name: 'Dallas Stars', location: 'Dallas', nickname: 'Stars' },
+						 { full_name: 'Minnesota Wild', location: 'Minnesota', nickname: 'Wild' },
+						 { full_name: 'Nashville Predators', location: 'Nashville', nickname: 'Predators' },
+						 { full_name: 'St. Louis Blues', location: 'St. Louis', nickname: 'Blues' },
+						 { full_name: 'Winnipeg Jets', location: 'Winnipeg', nickname: 'Jets' },
+
+						 { full_name: 'Boston Bruins', location: 'Boston', nickname: 'Bruins' },
+						 { full_name: 'Buffalo Sabres', location: 'Buffalo', nickname: 'Sabres' },
+						 { full_name: 'Detroit Red Wings', location: 'Detroit', nickname: 'Red Wings' },
+						 { full_name: 'Florida Panthers', location: 'Florida', nickname: 'Panthers' },
+						 { full_name: 'Montreal Canadiens', location: 'Montreal', nickname: 'Canadiens' },
+						 { full_name: 'Ottawa Senators', location: 'Ottawa', nickname: 'Senators' },
+						 { full_name: 'Tampa Bay Lightning', location: 'Tampa Bay', nickname: 'Lightning' },
+						 { full_name: 'Toronto Maple Leafs', location: 'Toronto', nickname: 'Maple Leafs' },
+
+						 { full_name: 'Carolina Hurricanes', location: 'Carolina', nickname: 'Hurricanes' },
+						 { full_name: 'Columbus Blue Jackets', location: 'Columbus', nickname: 'Blue Jackets' },
+						 { full_name: 'New Jersey Devils', location: 'New Jersey', nickname: 'Devils' },
+						 { full_name: 'New York Islanders', location: 'New York', nickname: 'Islanders' },
+						 { full_name: 'New York Rangers', location: 'New York', nickname: 'Rangers' },
+						 { full_name: 'Philadelphia Flyers', location: 'Philadelphia', nickname: 'Flyers' },
+						 { full_name: 'Pittsburgh Penguins', location: 'Pittsburgh', nickname: 'Penguins' },
+						 { full_name: 'Washington Capitals', location: 'Washington', nickname: 'Capitals' },
+
+	          ])
 
 # NBA Teams
 # TODO
