3bb6a62989db6bfbc955db5577432ead69207426
Paul Whalen <pgwhalen@gmail.com>
2013-09-03 23:20:52 -0400

    bit of work on user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,5 @@ class User < ActiveRecord::Base
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   before_save { self.email = email.downcase }
 
+  validates :password, length: { minimum: 6 }
 end
