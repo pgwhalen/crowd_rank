2e89541ba8bf7b11e31cbe3692592c3699c6fd04
Paul Whalen <pgwhalen@gmail.com>
2013-09-22 16:40:54 -0400

    TeamGroup#Show loads composite rankings directly rather than doing its own calculation

--- app/controllers/team_groups_controller.rb
+++ app/controllers/team_groups_controller.rb
@@ -1,49 +1,27 @@
+require "#{Rails.root}/app/helpers/date_helper"
+include DateHelper
+
 class TeamGroupsController < ApplicationController
 
 	def show
 		@team_group = TeamGroup.includes(:teams).find(params[:id])
+		@user_group = params[:user_group] ? UserGroup.find(params[:user_group]) : UserGroup.find_by_name("Everyone")
 
 		@series = []
 		# Load team data (nicknames, colors) from db
-		@team_group.teams.each do |team|
+		@team_group.teams.order(:nickname).each do |team|
 			@series << { id: team.id, name: team.nickname, color: team.primary_color, data: [] }
 		end
 
-		# Load ranking data from db
-		# All of this below business should happend on the write side, not read side
-		# the following line should instead fetch CompositeRankings
-		rankings = Ranking.includes(:ranks)
-
-		combined_rankings_hash = {}
-		rankings.each do |r|
-			week = r.created_at.beginning_of_week
-			if combined_rankings_hash[week]
-				combined_rankings_hash[week] << r
-			else
-				combined_rankings_hash[week] = [r]
-			end
-		end
-
-		# Ugly, ugly, ugly.  Temporary
+		# Load composed ranking data from db
+		composite_rankings = CompositeRanking.includes(:composite_ranks).where(team_group_id: @team_group.id, user_group_id: @user_group.id).order(:period_start_at)
 		@time_periods = []
-		combined_rankings_hash.sort.each do |week, rankings|
-			@time_periods << week.to_date.to_s
-			composite_ranking = {}
-			rankings.each do |ranking|
-				ranking.ranks.each do |rank|
-					team_id = rank.team_id
-					value = rank.value
-					if composite_ranking[team_id]
-						composite_ranking[team_id] += value
-					else
-						composite_ranking[team_id] = value
-					end
-				end
-			end
-			composite_ranking.each do |team_id, value|
-				value = value / rankings.count
-				@series.find { |s| s[:id] == team_id }[:data] << value
+		composite_rankings.each do |cr|
+			@time_periods << cr.period_start_at.to_date.strftime('%b %e')
+			cr.composite_ranks.each do |r|
+				@series.find { |s| s[:id] == r.team_id }[:data] << r.value
 			end
 		end
+
 	end
 end
